4335e0a0c9b42159befa1f64ebc1e44899f2e7a7
Ikko Yamane <yamane.ikko@gmail.com>
2018-11-17 00:53:17 +0900

    Add FirstSteps.

---
+++ FirstSteps/first_steps.hs
@@ -0,0 +1,19 @@
+import qualified Text.ParserCombinators.Parsec as Prs
+import System.Environment
+
+symbol :: Prs.Parser Char
+symbol = Prs.oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case Prs.parse symbol "Syntax Error." input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+spaces :: Prs.Parser ()
+spaces = Prs.skipMany1 Prs.space
+
+main :: IO()
+main = do
+        (expr:_) <- getArgs
+        putStrLn (readExpr expr)
+
