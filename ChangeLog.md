55fb1d26d69294be5683f4728f62fecbf4185fe7
Ikko Yamane <yamane.ikko@gmail.com>
2018-11-20 18:46:14 +0900

    Remove unnecessary stuff.

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,13 @@
 # Changelog for scheme-in-haskell
 
 ## Unreleased changes
+### Added
+- Add Parse.hs.
+- Stack project skeleton.
+
+### Changed
+- Fix import statements.
+
+### Removed
+- Untrack the .cabal file.
+
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,8 +1,8 @@
-import 01_introduction.LispVal
-import Text.Parsec
-import Text.Parsec.Text
+import LispVal
+import qualified Text.Parsec as Prs
+import qualified Text.Parsec.Text as T
 
-newtype Parser LispVal = Parser (Text -> [(LispVal, Text)])
+newtype Parser LispVal = Parser (T.Text -> [(LispVal, T.Text)])
 
 main :: IO ()
 main = do
