f207878ea935ab3a7d0df638562f7bec0e64b682
Ikko Yamane <yamane.ikko@gmail.com>
2018-11-21 16:04:56 +0900

    Add a test for SimpleParser.

--- src/SimpleParser.hs
+++ src/SimpleParser.hs
@@ -1,5 +1,6 @@
 module SimpleParser (
     symbol
+    , symbols
     , readExpr
     , spaces
 ) where
@@ -7,13 +8,16 @@ module SimpleParser (
 import qualified Text.ParserCombinators.Parsec as Prs
 import System.Environment
 
+symbols :: [Char]
+symbols = "!#$%&|*+-/:<=>?@^_~"
+
 symbol :: Prs.Parser Char
-symbol = Prs.oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = Prs.oneOf symbols
 
 readExpr :: String -> String
 readExpr input = case Prs.parse symbol "Syntax Error." input of
     Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+    Right val -> "Found value: " ++ show val
 
 spaces :: Prs.Parser ()
 spaces = Prs.skipMany1 Prs.space
